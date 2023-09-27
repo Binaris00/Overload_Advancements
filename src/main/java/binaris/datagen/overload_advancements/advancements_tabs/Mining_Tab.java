b203ef19a4a078da34e3eb825b751b7b69a34aec
Binaris00 <binaris000@gmail.com>
2023-09-27 17:47:31 -0400

    New advancements for mining tab

--- src/main/java/binaris/datagen/overload_advancements/advancements_tabs/Mining_Tab.java
+++ src/main/java/binaris/datagen/overload_advancements/advancements_tabs/Mining_Tab.java
@@ -4,9 +4,9 @@ import binaris.datagen.overload_advancements.AdvancementProvider;
 import net.minecraft.advancement.Advancement;
 import net.minecraft.advancement.AdvancementEntry;
 import net.minecraft.advancement.AdvancementFrame;
-import net.minecraft.advancement.AdvancementRewards;
-import net.minecraft.advancement.criterion.ConsumeItemCriterion;
+import net.minecraft.advancement.criterion.EnterBlockCriterion;
 import net.minecraft.advancement.criterion.InventoryChangedCriterion;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.Items;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
@@ -20,10 +20,10 @@ public class Mining_Tab implements Consumer<Consumer<AdvancementEntry>> {
         AdvancementEntry rootAdvancement = Advancement.Builder.create()
                 .display(
                         Items.COBBLESTONE,
-                        Text.literal("Your First Cobblestone Block"),
-                        Text.literal("Maybe it's time to make a pickaxe!!"),
+                        Text.literal("Your first cobblestone block!"),
+                        Text.translatable("Good material to make a pickaxe"),
                         new Identifier("textures/gui/advancements/backgrounds/stone.png"),
-                        AdvancementFrame.TASK, // Options: TASK, CHALLENGE, GOAL
+                        AdvancementFrame.TASK,
                         true,
                         true,
                         false
@@ -31,49 +31,89 @@ public class Mining_Tab implements Consumer<Consumer<AdvancementEntry>> {
                 .criterion("got_cobblestone", InventoryChangedCriterion.Conditions.items(Items.COBBLESTONE))
                 .build(advancementConsumer, AdvancementProvider.MOD_ID + "/root");
 
-        Advancement craftPickaxe = Advancement.Builder.create()
+        AdvancementEntry craftStonePickaxe = Advancement.Builder.create().parent(rootAdvancement)
                 .display(
                         Items.STONE_PICKAXE,
-                        Text.literal("The perfect tool!"),
-                        Text.literal("Conquer all the world"),
+                        Text.literal("The loyal tool!"),
+                        Text.literal("Upgrade your pickaxe"),
                         null,
                         AdvancementFrame.TASK,
                         true,
                         true,
                         false
                 )
-                .criterion("craftPickaxe", InventoryChangedCriterion.Conditions.items(Items.STONE_PICKAXE))
-                .build(advancementConsumer, AdvancementProvider.MOD_ID + "/craft_pickaxe").value();
+                .criterion("craft_stone_pickaxe", InventoryChangedCriterion.Conditions.items(Items.STONE_PICKAXE))
+                .build(advancementConsumer, AdvancementProvider.MOD_ID + "/craft_stone_pickaxe");
 
-
-        Advancement gotOakAdvancement = Advancement.Builder.create().parent(rootAdvancement)
+        AdvancementEntry craftIronPickaxe = Advancement.Builder.create().parent(craftStonePickaxe)
                 .display(
-                        Items.OAK_LOG,
-                        Text.literal("Your First Oak Block"),
-                        Text.literal("Bare fisted"),
-                        null, // children to parent advancements don't need a background set
+                        Items.IRON_PICKAXE,
+                        Text.literal("A necessary update"),
+                        Text.literal("Perfect to mine other ores"),
+                        null,
                         AdvancementFrame.TASK,
                         true,
                         true,
                         false
                 )
-                .rewards(AdvancementRewards.Builder.experience(1000))
-                .criterion("got_wood", InventoryChangedCriterion.Conditions.items(Items.OAK_LOG))
-                .build(advancementConsumer, AdvancementProvider.MOD_ID + "/got_wood").value();
+                .criterion("craft_iron_pickaxe", InventoryChangedCriterion.Conditions.items(Items.IRON_PICKAXE))
+                .build(advancementConsumer, AdvancementProvider.MOD_ID + "/craft_iron_pickaxe");
 
-        Advancement eatAppleAdvancement = Advancement.Builder.create().parent(rootAdvancement)
+        Advancement placeFurnace = Advancement.Builder.create().parent(craftStonePickaxe)
                 .display(
-                        Items.APPLE,
-                        Text.literal("Apple and Beef"),
-                        Text.literal("Ate an apple and beef"),
-                        null, // children to parent advancements don't need a background set
-                        AdvancementFrame.CHALLENGE,
+                        Items.FURNACE,
+                        Text.literal("Now, where are the cookies?"),
+                        Text.literal("Used to smelt whatever you want"),
+                        null,
+                        AdvancementFrame.TASK,
                         true,
                         true,
                         false
                 )
-                .criterion("ate_apple", ConsumeItemCriterion.Conditions.item(Items.APPLE))
-                .criterion("ate_cooked_beef", ConsumeItemCriterion.Conditions.item(Items.COOKED_BEEF))
-                .build(advancementConsumer, AdvancementProvider.MOD_ID + "/ate_apple_and_beef").value();
+                .criterion("placeFurnace", EnterBlockCriterion.Conditions.block(Blocks.FURNACE))
+                .build(advancementConsumer, AdvancementProvider.MOD_ID + "/place_furnace").value();
+
+
+        AdvancementEntry getCoal = Advancement.Builder.create().parent(rootAdvancement)
+                .display(
+                        Items.COAL,
+                        Text.translatable("The underrated ore"),
+                        Text.translatable("Use with a furnace"),
+                        null,
+                        AdvancementFrame.TASK,
+                        true,
+                        true,
+                        false
+                )
+                .criterion("get_coal", InventoryChangedCriterion.Conditions.items(Items.COAL))
+                .build(advancementConsumer, AdvancementProvider.MOD_ID + "/get_coal");
+
+        Advancement getIron = Advancement.Builder.create().parent(getCoal)
+                .display(
+                        Items.IRON_INGOT,
+                        Text.translatable("Always needed ore"),
+                        Text.translatable(""),
+                        null,
+                        AdvancementFrame.TASK,
+                        true,
+                        true,
+                        false
+                )
+                .criterion("get_iron", InventoryChangedCriterion.Conditions.items(Items.RAW_IRON))
+                .build(advancementConsumer, AdvancementProvider.MOD_ID + "/get_iron").value();
+
+        Advancement getCopper = Advancement.Builder.create().parent(getCoal)
+                .display(
+                        Items.COPPER_INGOT,
+                        Text.translatable("Electric conductor"),
+                        Text.translatable("And, how can I use this?"),
+                        null,
+                        AdvancementFrame.TASK,
+                        true,
+                        true,
+                        false
+                )
+                .criterion("get_copper", InventoryChangedCriterion.Conditions.items(Items.RAW_COPPER))
+                .build(advancementConsumer, AdvancementProvider.MOD_ID + "/get_copper").value();
     }
 }
